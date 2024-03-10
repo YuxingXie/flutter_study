bf965952885100f70442d690f56c03cf748d851f
xieyuxing <185246042@qq.com>
2024-03-11 03:25:40 +0800

    222

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,5 @@ main() async {
       await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
-
   print(await http.read(Uri.https('example.com', 'foobar.txt')));
 }
